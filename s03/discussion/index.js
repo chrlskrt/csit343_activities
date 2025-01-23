55644f7e2126b93ae4eaa623be8cbe0f20e36e01
Charlene Repuesto <repuestocharlene@gmail.com>
2025-01-23 14:30:27 +0800

    Add discussion code for JS variables and log statements

---
+++ s03/discussion/index.js
@@ -0,0 +1,44 @@
+console.log("Hello World!");
+console.log("This is our first JS session");
+
+// Comments
+  // ctrl + /
+  // to describe the written code
+  // Documentation:
+  // code readability
+  // maintainability
+  // collaboration
+
+  /*
+    These are two types of comments 
+    1. single-line comments denoted by two slashes
+    2. multi-line comments denoted by slash and asterisk
+  */
+
+// Variables
+  // to contain data within a program.
+
+let myName = "Charlene";
+
+console.log(myName);
+
+// Variable Declaration
+let newVariable;
+console.log(newVariable);
+
+// we use assignment (=) operator to assign/reassign a value
+let myPet = "Bantay";
+console.log(myPet);
+
+// Reference error
+// console.log(hello);
+// let hello;
+
+// Declare the variable first before using it
+// let hello;
+// console.log(hello);
+
+// console.log(hello)
+// var hello = "greeting"; 
+
+// Variable hoisting - var will be placed at the top of the current scope
